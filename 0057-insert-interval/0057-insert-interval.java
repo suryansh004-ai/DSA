241784298db4c4d5f801c8568b5f527f71afc077
suryansh  17 <kushansh004@gmail.com>
2026-07-02 00:14:08 +0530

    Time: 1 ms (97.92%), Space: 47.1 MB (36.89%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,38 +1,50 @@
+import java.util.*;
+
 class Solution {
     public int[][] insert(int[][] intervals, int[] ne) {
-        List<int[]> list= new ArrayList<>();
+
+        List<int[]> list = new ArrayList<>();
         List<int[]> ans = new ArrayList<>();
+
         boolean ins = false;
-         
-         for(int []i :intervals)
-{
-    if(!ins &&  ne[0]< i[0])
-    {
-list.add(ne);
-ins = true;
+
+        // Step 1: Insert the new interval
+        for (int[] interval : intervals) {
+
+            if (!ins && ne[0] <= interval[0]) {
+                list.add(ne);
+                ins = true;
+            }
+
+            list.add(interval);
+        }
+
+        // If new interval has the largest start
+        if (!ins) {
+            list.add(ne);
+        }
+
+        // Step 2: Merge intervals
+        int s1 = list.get(0)[0];
+        int e1 = list.get(0)[1];
+
+        for (int i = 1; i < list.size(); i++) {
+
+            int s2 = list.get(i)[0];
+            int e2 = list.get(i)[1];
+
+            if (s2 <= e1) {
+                e1 = Math.max(e1, e2);
+            } else {
+                ans.add(new int[]{s1, e1});
+                s1 = s2;
+                e1 = e2;
+            }
+        }
+
+        // Add the last merged interval
+        ans.add(new int[]{s1, e1});
+
+        return ans.toArray(new int[ans.size()][]);
     }
-    list.add(i);
-
-}  
-if(!ins)list.add(ne);
-
-  int s1 = list.get(0)[0];
-  int e1 = list.get(0)[1];
-
-   for(int i =1 ;i< list.size();i++ )
-   {
-      int s2 = list.get(i)[0];
-  int e2 = list.get(i)[1];
-  if(s2<= e1)
-  e1 = Math.max(e1,e2);
-  else {
-    ans.add(new int[]{s1,e1});
-    s1=s2;
-    e1 =e2;
-
-  }
-   
-   } 
-   ans.add(new int[]{s1,e1});
-   return ans.toArray(new int[ans.size()][]);}
 }
