60aa26cac6c4a619da9dbb6465222887545302c6
suryansh  17 <kushansh004@gmail.com>
2026-07-01 21:08:18 +0530

    Time: 3 ms (99.65%), Space: 47.6 MB (18.93%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] intervalIntersection(int[][] fi, int[][] se) {
+    List<int[]>ans = new ArrayList<>();
+    int i =0,j=0;
+    while(i<fi.length && j<se.length){
+        int  s = Math.max(fi[i][0],se[j][0]);
+        int e = Math.min(fi[i][1],se[j][1]);
+
+        if(s<=e){
+            ans.add(new int[]{s ,e});
+  }
+if(fi[i][1]<se[j][1]) i++;
+else j++;
+    }
+    return ans.toArray(new int[ans.size()][]);
+     }
+}
