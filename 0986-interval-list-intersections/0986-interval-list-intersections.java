eb9c37c0062b1c458133031fc9dadaf82dc67117
suryansh  17 <kushansh004@gmail.com>
2026-07-01 21:22:35 +0530

    Time: 4 ms (49.47%), Space: 45.2 MB (100%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,25 +1,24 @@
 class Solution {
-    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+    public int[][] intervalIntersection(int[][] fi, int[][] se) {
 
         List<int[]> ans = new ArrayList<>();
 
-        int i = 0;
-        int j = 0;
+        int i = 0 ,j = 0;
 
-        while (i < firstList.length && j < secondList.length) {
+        while (i < fi.length && j < se.length) {
 
-            int start = Math.max(firstList[i][0], secondList[j][0]);
-            int end = Math.min(firstList[i][1], secondList[j][1]);
+            int s = Math.max(fi[i][0], se[j][0]);
+            int e = Math.min(fi[i][1], se[j][1]);
 
-            if (start <= end) {
-                ans.add(new int[]{start, end});
-            }
+            if (s <= e) 
+                ans.add(new int[]{s, e});
+            
 
-            if (firstList[i][1] < secondList[j][1]) {
+            if (fi[i][1] < se[j][1]) 
                 i++;
-            } else {
+             else 
                 j++;
-            }
+            
         }
 
         return ans.toArray(new int[ans.size()][]);
